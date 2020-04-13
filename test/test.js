de170f8182129440242da0a87b31da91053e5926
Brad Cordeiro <brad.cordeiro@gmail.com>
2020-04-13 17:26:27 +0000

    use streams in edl processing

--- test/test.js
+++ test/test.js
@@ -1,3 +1,4 @@
 require('../lib/EditDecisionList/EditDecisionList.test');
 require('../lib/MotionEffect/MotionEffect.test');
 require('../lib/Event/Event.test');
+require('../lib/CMX3600Parser/CMX3600Parser.test');
